6675f95513c3f3b21e131b42340b23e897c492bb
b7Rexx <bij.maharjan@gmail.com>
2020-02-10 17:42:27 +0545

    travis warning fixes

--- src/component/list-item.js
+++ src/component/list-item.js
@@ -10,13 +10,13 @@ class ListItem extends React.Component {
     return (
       <li className='list-item'>
         <a className={'check-todo ' + (this.props.complete ? 'btn-completed' : '')}
-           onClick={this.props.completedEvent}>
+           onClick={this.props.completedEvent} href='#'>
           <i className={'fa ' + (this.props.complete ? 'fa-check-circle' : 'fa-circle')}/>
         </a>
         <span className={this.props.complete ? 'completed' : ''}
               onKeyDown={this.props.editEvent}
               onClick={(e) => this.contentEdit(e)}>{this.props.item}</span>
-        <a className='remove-todo'
+        <a className='remove-todo' href='#'
            title='remove' onClick={this.props.removeEvent}>
           <i className='fa fa-trash'/>
         </a>
