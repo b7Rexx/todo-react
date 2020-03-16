deada72752bca6d7dd58e14c14bcf62a77440302
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-16 07:55:48 +0000

    Deploy b7Rexx/todo-react to github.com/b7Rexx/todo-react.git:gh-pages

---
+++ precache-manifest.82960e92187d5759e84aee644fe9fe91.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb48a319a14729c02807af6ddbaba628",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "8f94f320a582f75eb5d6",
+    "url": "/todo-react/static/css/2.9e9c77e2.chunk.css"
+  },
+  {
+    "revision": "b310618535f8dd08c26a",
+    "url": "/todo-react/static/css/main.cd80f04f.chunk.css"
+  },
+  {
+    "revision": "8f94f320a582f75eb5d6",
+    "url": "/todo-react/static/js/2.e2d76165.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todo-react/static/js/2.e2d76165.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b310618535f8dd08c26a",
+    "url": "/todo-react/static/js/main.ab9698f2.chunk.js"
+  },
+  {
+    "revision": "11e9f614a99bf356d50c",
+    "url": "/todo-react/static/js/runtime-main.5100c8e9.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/todo-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/todo-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/todo-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/todo-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/todo-react/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
